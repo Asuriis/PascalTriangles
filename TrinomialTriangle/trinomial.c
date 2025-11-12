fdc7a27ff021e83bf94d5addb7b1865c58e8a1b9
e2401862 <e2401862@WA2029-08-OLD>
2025-11-12 11:46:57 +0200

    Add project files.

---
+++ TrinomialTriangle/trinomial.c
@@ -0,0 +1,12 @@
+int trinomial(int a[]) {
+
+}
+
+/*
+to do
+adds 3 numbers above, (row=n, each row has k entries)
+rows are indexed from -k -> k, where |-k| = k
+e.g. for nth row (say 4), k spans -3 -> 3 (total of 7 entries)
+so k max or min can be expresses as n-1
+value of entry in kth spot can be expressed as k=
+*/
